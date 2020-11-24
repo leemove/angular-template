47d78386a3781f3651306e82101770542d9a3fe4
leemove <hg_limu@163.com>
2020-11-24 15:04:25 +0800

    init

---
+++ src/app/core/user/models/user.ts
@@ -0,0 +1,9 @@
+/**
+ * 用户信息
+ */
+export interface User {
+    /**
+     * 用户名
+     */
+    name: string;
+}
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from './models';
+import { tap } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  /**
+   * 用户信息
+   */
+  user?: User;
+
+  /**
+   * 不需要账号密码的登录方式
+   */
+  loginWithoutAccount(): Observable<User>{
+    return of({
+      name: 'a'
+    })
+    .pipe(
+      tap(user => this.user = user)
+    );
+  }
+
+  constructor() { }
+
+}
